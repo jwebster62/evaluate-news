d805e90437a5cb88c0fda74537aa603ec6f9b170
Joshua Webster <darkwolf62@gmail.com>
2020-07-25 12:33:46 -0500

    Keep up

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,13 +4,20 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'production',
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
