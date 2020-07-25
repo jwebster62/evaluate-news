9b3f5f227dbb1cd37d1c9af145cda23a414ffb72
Joshua Webster <darkwolf62@gmail.com>
2020-07-25 12:53:00 -0500

    changed port for express server

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,10 +6,10 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8081/test')
         .then(res => res.json())
         .then(function(res) {
-            document.getElementById('results').innerHTML = res.message
+            document.getElementById('results').innerHTML = data.message
         })
 }
 
