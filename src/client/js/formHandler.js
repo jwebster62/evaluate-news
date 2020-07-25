643ac4e4f99f6ba8289191f7da420c36d8e396c1
Joshua Webster <darkwolf62@gmail.com>
2020-07-25 14:41:50 -0500

    i forget

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,4 @@
-const myKey = "a61e1677e6deb8dba178e66a9a9ae65b"
-
-
-/*function handleSubmit(event) {
+function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
@@ -17,33 +14,3 @@ const myKey = "a61e1677e6deb8dba178e66a9a9ae65b"
 }
 
 export { handleSubmit }
-*/
-const https = require('follow-redirects').https;
-const fs = require('fs');
-
-const options = {
-    'method': 'POST',
-    'hostname': 'api.meaningcloud.com',
-    'path': `/sentiment-2.1?key=${myKey}&lang=<lang>&txt=<text>&model=<model>`,
-    'headers': {},
-    'maxRedirects': 20
-};
-
-const req = https.request(options, function(res) {
-    const chunks = [];
-
-    res.on("data", function(chunk) {
-        chunks.push(chunk);
-    });
-
-    res.on("end", function(chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-    });
-
-    res.on("error", function(error) {
-        console.error(error);
-    });
-});
-
-req.end();
